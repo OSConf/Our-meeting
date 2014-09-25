0cdcbb6b9a619056f8777dfea267c8e777e20ad2
Gabs00 <gewen87@gmail.com>
2014-09-25 12:56:53 -0400

    Adding chat support

---
+++ app/components/WebRTC/rtcchat.js
@@ -0,0 +1,31 @@
+function ChatSetup(user, chat, store){
+
+  chat.onmessage = function(message){
+    var item = {};
+    item[user] = message.data;
+    store.push(item);
+    console.log(message.data);
+  };
+
+  chat.onopen = function(evt){
+    var item = {};
+    item[user] = 'joined the room';
+    store.push(item);
+
+  };
+
+  chat.onclose = function(evt){
+    var item = {};
+    item[user] = 'left the room';
+    store.push(item);
+
+  };
+
+  chat.onerror = function(evt){
+    var item = {};
+    item[user] = evt;
+    store.push(item);
+    console.log(evt);
+  };
+  return chat;
+}
